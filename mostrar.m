b6016a9a24e89b700a6016ed93f18ea35157903d
dromme <melibarba4@gmail.com>
2017-09-18 22:06:34 -0500

    El tercero

--- mostrar.m
+++ mostrar.m
@@ -17,7 +17,11 @@ start(vid);
 
 %preview(vid);
 %Inicializar las posiciones de los bloques
- x2=20;y2=20;
+x1=20;y1=0;
+x2=490;y2=0;
+%tamanobloques =  DEspues poner a dc2 random entre ciertos numeros, para que los bloques cambien de tamaño
+vel=10;
+
 while(vid.FramesAcquired <= 10000)
     
     
@@ -39,33 +43,60 @@ while(vid.FramesAcquired <= 10000)
     if isempty(s)
         
     else
-            %id = find([s.Area] > 1000 & [s.Area] < 100000 );
-            [~, id] = max([s.Area]);  % Quiero que siga los objetos más de taltal y el
-            hold on
-            %plot(s(id).Centroid(1),s(id).Centroid(2),'wp','MarkerSize',20,'MarkerFaceColor','r');
-           
-            x = s(id).Centroid(1) - 5;
-            y = s(id).Centroid(2) - 5;
-            po = [x, y, 13, 13];
-            r1 = rectangle('Position',po,'EdgeColor','r','LineWidth',2);
-            
-            
+        %id = find([s.Area] > 1000 & [s.Area] < 100000 );
+        [~, id] = max([s.Area]);  % Quiero que siga los objetos más de taltal y el
+        hold on
+        %plot(s(id).Centroid(1),s(id).Centroid(2),'wp','MarkerSize',20,'MarkerFaceColor','r');
+        
+        % Game Ball.
+        x = s(id).Centroid(1) - 5;
+        y = s(id).Centroid(2) - 5;
+        p = [x, y, 13, 13];
+        
+        % Bloques.
+        p1 = [x1, y1, 160, 40];
+        p2 = [x2, y2, 160, 50];
+        
+        %Dibujar
+        b1 = rectangle('Position',p1,'EdgeColor','g','LineWidth',1);
+        b2 = rectangle('Position',p2,'EdgeColor','g','LineWidth',1);
+        %Dibujar
+        r = rectangle('Position',p,'EdgeColor','r','LineWidth',2);
+        
+        %¿Chocan?
+        area2 = bboxOverlapRatio(p,p1);
+        area3 = bboxOverlapRatio(p,p2);
+        
+        if area2 > 0
+            %     Stop the game and display GAME OVER.
+            text(x,y,'GAME OVER','Color','r','FontSize', 20);
+        end
+        
+        if area3 > 0
+            %     Stop the game and display GAME OVER.
+            text(x,y,'GAME OVER','Color','r','FontSize', 20);
+        end
+        
+        
+        
+        
         %disp(['Center location is (',num2str(s(id).Centroid(1),4),', ',num2str(s(id).Centroid(2),4),')'])
     end
     
     
-    %%Aquí van los bloques
-   
-    po2 = [x2, y2, 120, 60];
-    r2 = rectangle('Position',po2,'EdgeColor','b','LineWidth',2,'FaceColor','b');
-    
-%     hRect = plot(x, y, '-k'); % Stores handle to plotted lines
     
     hold off
+    
     if y2>480
         y2=0;
     else
-        y2=y2+20;
+        y2=y2+vel;
+    end
+    
+    if y1>480
+        y1=0;
+    else
+        y1=y1+vel;
     end
     
     
--- p.m
+++ p.m
@@ -1,22 +1,46 @@
 clear all; close all; clc
-objects = imaqfind %find video input objects in memory
-delete(objects) %delete a video input object from memory
 
-vid=videoinput('winvideo',1,'YUY2_640x480');
-set(vid,'ReturnedColorSpace','rgb');
+%  Bird (rectangle).
+% Position.
+xr = 30;  yr = 50;
+xr2 = 26; yr2 = 52;
+xr3 = 30; yr3 = 60;
+
+% Length of rectangle edges.
+deltaxr = 10; deltayr = 10;
+deltaxr2 = 5; deltayr2 = 5;
+deltaxr3 = 5; deltayr3 = 5;
+
+for k = 1:9
+
+% Vector for rectangle object.
+vr = [xr, yr, deltaxr, deltayr];
+vr2 = [xr2, yr2, deltaxr2, deltayr2];
+vr3 = [xr3, yr3, deltaxr3, deltayr3];
+
+% Draw objects.
+re1 = rectangle('Position',vr,'LineWidth',1);
+re2 = rectangle('Position',vr2,'LineWidth',1,'EdgeColor','g');
+re3 = rectangle('Position',vr3,'LineWidth',1,'EdgeColor','b');
+
+area2 = bboxOverlapRatio(vr,vr2);
+area3 = bboxOverlapRatio(vr,vr3);
+
+% if area > 0 
+% %     Stop the game and display GAME OVER.
+%     text(xr,yr,'GAME OVER','Color','b','FontSize', 20);
+% end
+if area > 0 
+%     Stop the game and display GAME OVER.
+    text(xr,yr,'GAME OVER','Color','g','FontSize', 20);
+end
 
 
-vid.FrameGrabInterval = 1;
-set(vid,'FramesPerTrigger',Inf);
-start(vid);
 
-set(vid,'ReturnedColorSpace','rgb');
-cdt = getsnapshot(vid);
+pause(0.5)
 
+rx = randi(80); 
+ xr = xr+2;  
+yr = yr+6;  
+end
 
-         cdt2 = flip(cdt,2);
- imwrite(cdt2,'myfoto4.png');
-%     [BW,maskedRGBImage] = createMask(cdt2);
-% imwrite(BW,'myfoto2mk.png');
-
-%imshow(BW);
